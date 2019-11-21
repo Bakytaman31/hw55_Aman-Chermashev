7b9fcfd41b0f4cd114d4f98768bad6cbefc543a7
Aman Chermashev <bakytaman228337@gmail.com>
2019-11-21 16:22:36 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Remote from "./Componets/Remote/Remote";
 
 class App extends Component {
+    state = {
+      ingridients:[
+          {name: 'Meat', count: 0},
+          {name: 'Cheese', count: 0},
+          {name: 'Salad', count: 0},
+          {name: 'Bacon', count: 0},
+      ],
+      totalPrice: 20
+    };
+
+    addIngridient = ingridient => {
+        const ingridients = [...this.state.ingridients];
+        const ingridientsNames = this.state.ingridients.findIndex(i => i.name === ingridient.name);
+        ingridients[ingridientsNames].count++;
+        let totalPrice = this.state.totalPrice;
+        totalPrice += ingridient.price;
+        this.setState({ingridients, totalPrice});
+    };
+
+    removeIngridient = ingridient => {
+        const ingridients = [...this.state.ingridients];
+        const ingridientsNames = this.state.ingridients.findIndex(i => i.name === ingridient.name);
+        ingridients[ingridientsNames].count--;
+        let totalPrice = this.state.totalPrice;
+        totalPrice -= ingridient.price;
+        this.setState({ingridients, totalPrice});
+    };
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Remote
+              ingridientQuantity={this.state.ingridients}
+              price={this.state.totalPrice}
+              addIngr={this.addIngridient}
+              removeIngridient={this.removeIngridient}/>
+          <div className="Burger">
+              <div className="BreadTop">
+                  <div className="Seeds1"></div>
+                  <div className="Seeds2"></div>
+              </div>
+              <div className="BreadBottom"></div>
+              <p>Total Price: {this.state.totalPrice}</p>
+          </div>
       </div>
     );
   }
